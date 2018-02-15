6ca116402fbd895ea983a51c0a1a06c61a0868d1
Sahir <shehzadss@vcu.edu>
2018-02-15 09:50:50 -0800

    first

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var fileData = require("./app/data/friends.js")
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
